a5c024e4f31b01a121a538bf0f8f290b6f67bc95
Ubuntu <ubuntu@ip-172-31-81-175.ec2.internal>
2019-11-16 20:04:24 +0000

    remove unwantedfile

--- database.php
+++
@@ -1,9 +0,0 @@
-<?php
-$url='localhost';
-$username='root';
-$password='root';
-$conn=mysqli_connect($url,$username,$password,"crud");
-if(!$conn){
- die('Could not Connect My Sql:' .mysql_error());
-}
-?>
